ad76f81b87e14aae9e60721c0515ec7c0ee96682
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-08-27 13:46:54 -0500

    change in server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 3001;
 
-app.get("*", (req, res) => {
-    res.send("hello");
-});
+app.get("/", (req, res) =>
+res.sendFile(path.join(__dirname, "../client/build/index.html"))
+);
 
 app.listen(PORT, () => console.log("Listening at http://localhost:"+PORT+"/"))
